e67ebd2abaf757631d9a0a8fdf4bfed389238bfa
DaniilKhlestkov <khlestkov00@inbox.ru>
2023-11-23 18:50:47 +0300

    yes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld helloWorld = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloWorld1 = (HelloWorld) applicationContext.getBean("Helloworld");
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("helloWorld равен helloWorld1 ?"+(helloWorld == helloWorld1));
+        System.out.println("cat1 равен cat2 ? " + (cat == cat1));
+
+
     }
 }
